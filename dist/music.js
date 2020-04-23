48fe108f3ee212c591b6a738ecb52b472a2eca02
wlf113 <2638885993@qq.com>
2020-04-23 20:52:49 +0800

    Site updated: 2020-04-23 20:52:48

--- dist/music.js
+++ dist/music.js
@@ -4,16 +4,16 @@ const ap = new APlayer({
     autoplay: true,
     audio: [
       {
-        name: 'Gypsophila',
-        artist: 'MoreanP',
-        url: 'http://music.163.com/song/media/outer/url?id=517346741.mp3',
-        cover: '/images/music/Gypsophila.jpg',
-      },
-    {
         name: '星茶会',
         artist: '灰澈',
         url: 'http://music.163.com/song/media/outer/url?id=492390949.mp3',
         cover: '/images/music/星茶会.jpg',
+      },
+      {
+        name: 'Gypsophila',
+        artist: 'MoreanP',
+        url: 'http://music.163.com/song/media/outer/url?id=517346741.mp3',
+        cover: '/images/music/Gypsophila.jpg',
       },
     {
         name: 'Afterglow',
