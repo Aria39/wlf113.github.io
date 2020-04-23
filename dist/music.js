9344a5a9c16362095aa9780655ffcd41a34d88dd
wlf113 <2638885993@qq.com>
2020-04-23 20:17:51 +0800

    Site updated: 2020-04-23 20:17:50

--- dist/music.js
+++ dist/music.js
@@ -7,19 +7,19 @@ const ap = new APlayer({
         name: 'Gypsophila',
         artist: 'MoreanP',
         url: 'http://music.163.com/song/media/outer/url?id=517346741.mp3',
-        cover: '/images/music/Gypsophila.jpg',
+        cover: '/images/Gypsophila.jpg',
       },
     {
         name: '星茶会',
         artist: '灰澈',
         url: 'http://music.163.com/song/media/outer/url?id=492390949.mp3',
-        cover: '/images/music/星茶会.jpg',
+        cover: '/images/星茶会.jpg',
       },
     {
         name: 'Afterglow',
         artist: 'Saiakoup',
         url: 'http://music.163.com/song/media/outer/url?id=530397372.mp3',
-        cover: '/images/music/Afterglow.jpg',
+        cover: '/images/Afterglow.jpg',
       }
     ]
 });
